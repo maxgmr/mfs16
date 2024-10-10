0734891290c3b11e6a6f93b3eab87478c548ba6c
maxgmr <mgilmour@maxgmr.ca>
2024-10-10 14:53:07 -0700

    helper tests

--- mfs16core/src/helpers.rs
+++ mfs16core/src/helpers.rs
@@ -17,3 +17,28 @@ pub fn combine_u8_le(h: u8, l: u8) -> u16 {
 fn combine_u8_helper(left_byte: u8, right_byte: u8) -> u16 {
     ((left_byte as u16) << 8) | (right_byte as u16)
 }
+
+#[cfg(test)]
+mod tests {
+    use pretty_assertions::assert_eq;
+
+    use super::*;
+
+    #[test]
+    fn test_split_word() {
+        let original_val = 0x1234;
+        let (hb, lb) = split_word(original_val);
+        assert_eq!(hb, 0x12);
+        assert_eq!(lb, 0x34);
+    }
+
+    #[test]
+    fn test_combine_u8() {
+        let high_byte = 0x12;
+        let low_byte = 0x34;
+        let combined_u16_be = combine_u8_be(high_byte, low_byte);
+        let combined_u16_le = combine_u8_le(high_byte, low_byte);
+        assert_eq!(combined_u16_be, 0x1234);
+        assert_eq!(combined_u16_le, 0x3412);
+    }
+}
