449feaef8b595c3d185589738babae2ac92fa534
maxgmr <mgilmour@maxgmr.ca>
2024-10-11 12:59:32 -0700

    impl flag setting/resetting

--- mfs16core/src/cpu/flag.rs
+++ mfs16core/src/cpu/flag.rs
@@ -1,6 +1,8 @@
 //! All functionality related to the CPU flags.
 use std::{default::Default, fmt::Display};
 
+use Flag::*;
+
 /// Enum to access the individual CPU flags.
 #[derive(Debug, Copy, Clone)]
 pub enum Flag {
@@ -13,13 +15,31 @@ pub enum Flag {
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Flags {
     /// The Carry flag.
-    /// TODO better docs
+    /// This flag is set when an arithmetic carry or borrow occurred during the arithmetic
+    /// operation.
     carry: bool,
 }
 impl Flags {
     /// Create new [Flags] set to the given values.
     pub fn new(carry: bool) -> Self {
         Self { carry }
+    }
+
+    /// Set the given [Flag].
+    pub fn set(&mut self, flag: Flag) {
+        self.change(flag, true);
+    }
+
+    /// Reset the given [Flag].
+    pub fn reset(&mut self, flag: Flag) {
+        self.change(flag, false);
+    }
+
+    /// Change the given [Flag] to the given boolean value.
+    pub fn change(&mut self, flag: Flag, val: bool) {
+        match flag {
+            Carry => self.carry = val,
+        }
     }
 }
 impl Default for Flags {
