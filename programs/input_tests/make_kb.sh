f127e73ddbdec03724ae5db1ce103ec1f896d5e8
maxgmr <mgilmour@maxgmr.ca>
2024-11-10 10:00:04 -0800

    add kb library

--- programs/input_tests/make_kb.sh
+++ programs/input_tests/make_kb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -f programs/kb
-cargo r -p mfs16assembler programs/input_tests/kb.mfs16 -o programs/kb
+cargo r -p mfs16assembler programs/kb_lib.mfs16 programs/input_tests/kb.mfs16 -o programs/kb
