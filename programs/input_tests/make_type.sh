af3d090aa2d56f2649a4d29bcf6265d4cffef42d
maxgmr <mgilmour@maxgmr.ca>
2024-11-10 17:16:47 -0800

    impl alphanumeric typing

--- programs/input_tests/make_type.sh
+++ programs/input_tests/make_type.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -f programs/type
-cargo r -p mfs16assembler programs/kb_lib.mfs16 programs/input_tests/type.mfs16 programs/fonts/5x7basic.mfs16 -o programs/type
+cargo r -p mfs16assembler programs/libs/kb_lib_h.mfs16 programs/input_tests/type.mfs16 programs/libs/kb_lib.mfs16 programs/fonts/5x7basic.mfs16 -o programs/type
